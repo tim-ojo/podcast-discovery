c6fb38e11d4ca023c06a0654480ea48e9e648926
Tim Ojo <ojo.tim@gmail.com>
2016-01-30 07:28:07 -0500

    implemented tests

--- app/tests/resource.server.model.test.js
+++ app/tests/resource.server.model.test.js
@@ -22,18 +22,98 @@ describe('Resource Model Unit Tests:', function() {
 
 	describe('Method Save', function() {
 		it('saves a new resource', function(done) {
-			done();
+			var podcast = new Resource({
+				title: '.Net Rocks',
+				subtitle: '.NET Rocks! is an Internet Audio Talk Show for Microsoft .NET Developers.',
+				type: 'podcast-audio',
+				url: 'http://www.dotnetrocks.com',
+				feedUrl: 'http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master',
+				authors: ['Carl Franklin', 'Richard Campbell'],
+				topics: [{topic: 'C#'}, {topic:'ASP .Net'}],
+				createdBy: 'Tim Ojo'
+			});
+
+			podcast.save(function(err, saved) {
+                should.not.exist(err);
+                done();
+            });
 		});
 
-		it('throws a validation error when the title is empty');
+		it('throws a validation error when the title is empty', function(done){
+			var resource = new Resource({
+				createdBy: 'Tim Ojo'
+			});
 
-		it('throws a validation error when the url is empty');
+			resource.save(function(err) {
+				should.exist(err);
+				err.errors.title.message.should.equal('Path `title` is required.');
+				done();
+			});
+		});
 
-		it('throws a validation error when the feedUrl is empty');
+		it('throws a validation error when the url is empty', function(done){
+			var resource = new Resource({
+				createdBy: 'Tim Ojo'
+			});
 
-		it('throws a validation error when the type is empty');
+			resource.save(function(err) {
+				should.exist(err);
+				err.errors.url.message.should.equal('Path `url` is required.');
+				done();
+			});
+		});
 
-		it('throws a validation error for a duplicate resource');
+		it('throws a validation error when the feedUrl is empty', function(done){
+			var resource = new Resource({
+				createdBy: 'Tim Ojo'
+			});
+
+			resource.save(function(err) {
+				should.exist(err);
+				err.errors.feedUrl.message.should.equal('Path `feedUrl` is required.');
+				done();
+			});
+		});
+
+		it('throws a validation error when the type is empty', function(done){
+			var resource = new Resource({
+				createdBy: 'Tim Ojo'
+			});
+
+			resource.save(function(err) {
+				should.exist(err);
+				err.errors.type.message.should.equal('Path `type` is required.');
+				done();
+			});
+		});
+
+		it('throws a validation error for a duplicate resource (by feedUrl)', function(done){
+			var podcast = new Resource({
+				title: '.Net Rocks',
+				type: 'podcast-audio',
+				url: 'http://www.dotnetrocks.com',
+				feedUrl: 'http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master'
+			});
+
+			podcast.save(function (err) {
+				should.not.exist(err);
+
+				var duplicate = new Resource({
+					title: '.Net Rocks',
+					type: 'podcast-audio',
+					url: 'http://www.dotnetrocks.com',
+					feedUrl: 'http://www.pwop.com/feed.aspx?show=dotnetrocks&filetype=master'
+				});
+
+				duplicate.save(function(err){
+					should.exist(err);
+					err.err.indexOf('duplicate key error').should.not.equal(-1);
+
+					done();
+				});
+			});
+
+		});
 
 	});
 

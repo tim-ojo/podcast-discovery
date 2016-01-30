658a1dfd0a0408fdb7a8ffa5aba25433f193955e
Tim Ojo <ojo.tim@gmail.com>
2016-01-30 06:22:41 -0500

    resource model 1st cut

---
+++ app/models/resource.server.model.js
@@ -0,0 +1,77 @@
+'use strict';
+
+/**
+ * Module dependencies.
+ */
+var mongoose = require('mongoose'),
+	Schema = mongoose.Schema;
+
+/**
+ * Topic Schema
+ */
+var TopicSchema = new Schema({
+	name: {type: String, required: true},
+	significance: Number
+});
+
+/**
+ * Resource Schema
+ */
+var ResourceSchema = new Schema({
+	title: {
+		type: String,
+		default: '',
+		required: true,
+		trim: true
+	},
+	subtitle: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	url: {
+		type: String,
+		default: '',
+		required: true,
+		trim: true
+	},
+	artworkUrl: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	feedUrl: {
+		type: String,
+		default: '',
+		required: true,
+		trim: true,
+		unique: true
+	},
+	description: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	lastPublishDate: {
+		type: Date,
+	},
+	authors: {
+		type: [String],
+		default: [],
+	},
+	topics: {
+		type: [TopicSchema]
+	},
+	createdBy: {
+		type: String,
+		default: '',
+		trim: true
+	},
+	createdOn: {
+		type: Date,
+		default: Date.now
+	}
+});
+
+mongoose.model('Topic', TopicSchema);
+mongoose.model('Resource', ResourceSchema);

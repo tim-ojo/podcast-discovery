8808bac7d0b69c7cefc24bd672a02af795603cb3
Tim Ojo <ojo.tim@gmail.com>
2016-02-02 07:45:05 -0500

    scaffolding the discover module

---
+++ public/modules/discover/config/core.client.routes.js
@@ -0,0 +1,16 @@
+'use strict';
+
+// Setting up route
+angular.module('discover').config(['$stateProvider', '$urlRouterProvider',
+	function($stateProvider, $urlRouterProvider) {
+		// Redirect to home view when route not found
+		$urlRouterProvider.otherwise('/');
+
+		// Home state routing
+		$stateProvider.
+		state('home', {
+			url: '/',
+			templateUrl: 'modules/discover/views/home.client.view.html'
+		});
+	}
+]);

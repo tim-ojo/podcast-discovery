68d1351ce27ae3d46690d82ec9db92be6822c10d
Tim Ojo <ojo.tim@gmail.com>
2016-03-09 17:23:50 -0500

    name change

--- public/modules/discover/controllers/allResources.client.controller.js
+++ public/modules/discover/controllers/allResources.client.controller.js
@@ -2,11 +2,11 @@
 
 angular.module('discover').controller('AllResourcesController', ['$scope', 'Resources', '$http',
 	function($scope, Resource, $http) {
-		//$scope.resources = Resource.query();
+		//$scope.items = Resource.query();
 
 		$scope.getResources = function () {
         $http.get('/resourceList/' + $scope.currentPage).success(function (response) {
-            $scope.resources = response;
+            $scope.items = response;
         });
 
 				$http.get('/resourcesCount').success(function (response) {

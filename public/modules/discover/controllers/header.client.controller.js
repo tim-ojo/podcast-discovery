25b4448e2f1fbbe0ee5f2fac4c563c48a47c5217
Tim Ojo <ojo.tim@gmail.com>
2016-03-11 05:17:59 -0500

    fix navbar collapse

--- public/modules/discover/controllers/header.client.controller.js
+++ public/modules/discover/controllers/header.client.controller.js
@@ -3,5 +3,14 @@
 angular.module('discover').controller('HeaderController', ['$scope', 'Authentication',
 	function($scope, Authentication) {
 		$scope.authentication = Authentication;
+
+		$scope.toggleCollapsibleMenu = function() {
+			$scope.isCollapsed = !$scope.isCollapsed;
+		};
+
+		// Collapsing the menu after navigation
+		$scope.$on('$stateChangeSuccess', function() {
+			$scope.isCollapsed = false;
+		});
 	}
 ]);

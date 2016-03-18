f09dfc9938a295570bf3a30ed9795de817f5c182
Tim Ojo <ojo.tim@gmail.com>
2016-03-18 14:01:56 -0400

    fix adding a new podcast to PD. Still some more work to be done

--- public/modules/discover/controllers/addResource.client.controller.js
+++ public/modules/discover/controllers/addResource.client.controller.js
@@ -1,19 +1,42 @@
 'use strict';
 
-angular.module('discover').controller('AddResourceController', ['$scope', 'Resources', '$state', '$stateParams', 'Authentication',
-	function($scope, Resource, $state, $stateParams, auth) {
+angular.module('discover').controller('AddResourceController', ['$scope', 'Resources', '$state', '$stateParams', 'Authentication', '$window',
+	function($scope, Resource, $state, $stateParams, auth, $window) {
 		$scope.resource = new Resource();
 
     // resource type options
+		/*
     $scope.resourceTypes = [{id: 'podcast-audio', name: 'podcast-audio'}, {id: 'podcast-video', name: 'podcast-video'}];
     $scope.selectedResourceType = $scope.resourceTypes[0];
     $scope.resource.type = $scope.selectedResourceType.name;
+		*/
 
     // defaults
     $scope.resource.createdBy = auth.user._id;
 
 		$scope.getRss = function(){
 			console.log($scope.resource.feedUrl);
+			$window.feednami.load($scope.resource.feedUrl, function(result){
+		    if(result.error){
+		      console.log(result.error);
+		    }
+		    else{
+		      var meta = result.feed.meta;
+		      console.log(meta);
+
+					$scope.resource.title = meta.title;
+					$scope.resource.subtitle = meta['itunes:subtitle']['#'];
+					$scope.resource.type = 'podcast-audio';
+					$scope.resource.url = meta.link;
+					$scope.resource.description = meta.description;
+					$scope.authors = meta.author;
+					$scope.topics = meta.categories.join(', ');
+					$scope.resource.artworkUrl = meta.image.url;
+					$scope.resource.lastPublishDate = meta.pubDate;
+
+					$scope.$apply();
+		    }
+		  });
 		};
 
     $scope.addResource = function(){
@@ -37,8 +60,10 @@ angular.module('discover').controller('AddResourceController', ['$scope', 'Resou
         });
       }
 
-      $scope.resource.$save(function(resp, headers){
-        $state.go('home'); // TODO: redirect this to the right place later
+      $scope.resource.$save(function(created, headers){
+				$state.go('podcast', {'resourceId': created._id});
+				//TODO; Go ahead and create the entries and send them as well
+				//TODO: On the server side be sure to index them in Elasticsearch
       },
       function(err){
         $scope.errorState = true;
